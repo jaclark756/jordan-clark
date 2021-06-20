a4c63f8230fff0b9a1d0ec762e5ba42505dc1ac7
jaclark7 <jaclark7@mccneb.edu>
2021-06-20 16:36:01 -0500

    added repo and site links

--- src/app/data.ts
+++ src/app/data.ts
@@ -2,32 +2,43 @@ export const PROJECTS = [
  {
     title: "Canvas Catalog",
     description: "project description",
-    imgUrl: "https://firebasestorage.googleapis.com/v0/b/jordan-clark-personal-site.appspot.com/o/images%2Fcatalog.PNG?alt=media&token=640debc2-fefe-4bfc-87e5-4a8fd9672567"
-    },
+    url: "https://www.mcccorporateonline.com/",
+    imgUrl: "https://firebasestorage.googleapis.com/v0/b/jordan-clark-personal-site.appspot.com/o/images%2Fcatalog.PNG?alt=media&token=640debc2-fefe-4bfc-87e5-4a8fd9672567",
+    repoUrl: "https://github.com/jaclark756/canvascatalogcode"      
+},
 
     {title: "Husqr Social Media App",
     description: "project description",
-    imgUrl: "https://firebasestorage.googleapis.com/v0/b/jordan-clark-personal-site.appspot.com/o/images%2Fhusqr.PNG?alt=media&token=bcf3f5db-ab28-4479-936b-157267739922"
-    },
+    url: "google.com",
+    imgUrl: "https://firebasestorage.googleapis.com/v0/b/jordan-clark-personal-site.appspot.com/o/images%2Fhusqr.PNG?alt=media&token=bcf3f5db-ab28-4479-936b-157267739922",
+    repoUrl: "https://gitlab.mccinfo.net/jaclark7/husqr-angular-java"      
+},
 
     {title: "ReciMe Wireframes",
     description: "project description",
-    imgUrl: "https://firebasestorage.googleapis.com/v0/b/jordan-clark-personal-site.appspot.com/o/images%2Fwireframes.PNG?alt=media&token=4dff8118-e635-484a-a473-af7b114625f1"
-    },
+    url: "https://whimsical.com/recipe-V7stC2wFKGkkDAKTYjP4QB",
+    imgUrl: "https://firebasestorage.googleapis.com/v0/b/jordan-clark-personal-site.appspot.com/o/images%2Fwireframes.PNG?alt=media&token=4dff8118-e635-484a-a473-af7b114625f1",
+},
 
     {title: "ReciMe Style Guide",
     description: "project description",
-    imgUrl: "https://firebasestorage.googleapis.com/v0/b/jordan-clark-personal-site.appspot.com/o/images%2Fstyleguide.PNG?alt=media&token=14fffdb9-226d-4903-9969-6d163662da00"
-    },
+    url: "https://styleguide-1a7ea.firebaseapp.com/",
+    imgUrl: "https://firebasestorage.googleapis.com/v0/b/jordan-clark-personal-site.appspot.com/o/images%2Fstyleguide.PNG?alt=media&token=14fffdb9-226d-4903-9969-6d163662da00",
+    repoUrl: "https://gitlab.mccinfo.net/cjcortney/recipe-app-styleguide"      
+},
 
     {title: "Movie Database Site",
     description: "project description",
-    imgUrl: "https://firebasestorage.googleapis.com/v0/b/jordan-clark-personal-site.appspot.com/o/images%2Fmovies.PNG?alt=media&token=1ea3fe28-62d7-4623-8599-2598cacbd766"
-    },
+    url: "https://mcc-movies.herokuapp.com/",
+    imgUrl: "https://firebasestorage.googleapis.com/v0/b/jordan-clark-personal-site.appspot.com/o/images%2Fmovies.PNG?alt=media&token=1ea3fe28-62d7-4623-8599-2598cacbd766",
+    repoUrl: "https://gitlab.mccinfo.net/cjcortney/movies-project"      
+},
 
     {title: "ReciMe",
     description: "project description",
-    imgUrl: "https://firebasestorage.googleapis.com/v0/b/jordan-clark-personal-site.appspot.com/o/images%2Frecime.PNG?alt=media&token=afe272ca-a769-4467-b414-54eccb51d03b"
-    },
+    url: "https://dev.recipe.mcc-code-school.com/",
+    imgUrl: "https://firebasestorage.googleapis.com/v0/b/jordan-clark-personal-site.appspot.com/o/images%2Frecime.PNG?alt=media&token=afe272ca-a769-4467-b414-54eccb51d03b",
+    repoUrl: "https://gitlab.mccinfo.net/code-school/students/recipe-app"    
+},
 ]
 
--- src/app/types/project.ts
+++ src/app/types/project.ts
@@ -2,7 +2,9 @@ export interface Project {
 
       title: string;
     description: string;
+    url: string;
     imgUrl: string;
     frameworks?: any[];
+    repoUrl?: string;
 
 }
