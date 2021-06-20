8330c6fc339db91e9ec0fc8203f8aed10791d5c6
jaclark7 <jaclark7@mccneb.edu>
2021-06-20 14:05:33 -0500

    added quote and hover effect on project image

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,7 +7,7 @@ import { ProjectDialogComponent } from '../project-dialog/project-dialog.compone
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
 
