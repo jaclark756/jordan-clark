bd7825b36cadeb8c90a05a907b924390f492f0e5
jaclark7 <jaclark7@mccneb.edu>
2021-06-21 10:54:58 -0500

    added links to dialog, app data

--- src/app/data.ts
+++ src/app/data.ts
@@ -1,44 +1,63 @@
 export const PROJECTS = [
- {
-    title: "Canvas Catalog",
-    description: "project description",
-    url: "https://www.mcccorporateonline.com/",
-    imgUrl: "https://firebasestorage.googleapis.com/v0/b/jordan-clark-personal-site.appspot.com/o/images%2Fcatalog.PNG?alt=media&token=640debc2-fefe-4bfc-87e5-4a8fd9672567",
-    repoUrl: "https://github.com/jaclark756/canvascatalogcode"      
-},
+    {
+        title: "CANVAS CATALOG",
+        description: "Storefront for non-credit workforce training opportunities.",
+        url: "https://www.mcccorporateonline.com/",
+        imgUrl: "https://firebasestorage.googleapis.com/v0/b/jordan-clark-personal-site.appspot.com/o/images%2Fcatalog.PNG?alt=media&token=640debc2-fefe-4bfc-87e5-4a8fd9672567",
+        repoUrl: "https://github.com/jaclark756/canvascatalogcode",
+        frameworks: ["HTML", "CSS", "JavaScript"]
+    },
 
-    {title: "Husqr Social Media App",
-    description: "project description",
-    url: "google.com",
-    imgUrl: "https://firebasestorage.googleapis.com/v0/b/jordan-clark-personal-site.appspot.com/o/images%2Fhusqr.PNG?alt=media&token=bcf3f5db-ab28-4479-936b-157267739922",
-    repoUrl: "https://gitlab.mccinfo.net/jaclark7/husqr-angular-java"      
-},
+    {
+        title: "HUSQR SOCIAL MEDIA APP",
+        description: "Think Twitter.  We used Spring Boot backend, Angular front end to allow users to create Husqs, find friends, and interact with other Husqrs.",
+        url: "google.com",
+        imgUrl: "https://firebasestorage.googleapis.com/v0/b/jordan-clark-personal-site.appspot.com/o/images%2Fhusqr.PNG?alt=media&token=bcf3f5db-ab28-4479-936b-157267739922",
+        repoUrl: "https://gitlab.mccinfo.net/jaclark7/husqr-angular-java",
+        frameworks: ["Angular", "HTML", "CSS", "Angular Material"]
+    },
 
-    {title: "ReciMe Wireframes",
-    description: "project description",
-    url: "https://whimsical.com/recipe-V7stC2wFKGkkDAKTYjP4QB",
-    imgUrl: "https://firebasestorage.googleapis.com/v0/b/jordan-clark-personal-site.appspot.com/o/images%2Fwireframes.PNG?alt=media&token=4dff8118-e635-484a-a473-af7b114625f1",
-},
+    {
+        title: "RECIME WIREFRAMES",
+        description: "Medium fidelity mockups for the ReciMe recipe sharing site. Wireframes for both mobile and web to make lighten the load of other developers on the project.",
+        url: "https://whimsical.com/recipe-V7stC2wFKGkkDAKTYjP4QB",
+        imgUrl: "https://firebasestorage.googleapis.com/v0/b/jordan-clark-personal-site.appspot.com/o/images%2Fwireframes.PNG?alt=media&token=4dff8118-e635-484a-a473-af7b114625f1",
+        frameworks: ["Medium Fidelity Mockup", "Whimsical"]
+    },
 
-    {title: "ReciMe Style Guide",
-    description: "project description",
-    url: "https://styleguide-1a7ea.firebaseapp.com/",
-    imgUrl: "https://firebasestorage.googleapis.com/v0/b/jordan-clark-personal-site.appspot.com/o/images%2Fstyleguide.PNG?alt=media&token=14fffdb9-226d-4903-9969-6d163662da00",
-    repoUrl: "https://gitlab.mccinfo.net/cjcortney/recipe-app-styleguide"      
-},
+    {
+        title: "RECIME STYLE GUIDE",
+        description: "Standalone application that breaks down color scheme, fonts, Angular Material components, and provides developers with the necessary code snippets.",
+        url: "https://styleguide-1a7ea.firebaseapp.com/",
+        imgUrl: "https://firebasestorage.googleapis.com/v0/b/jordan-clark-personal-site.appspot.com/o/images%2Fstyleguide.PNG?alt=media&token=14fffdb9-226d-4903-9969-6d163662da00",
+        repoUrl: "https://gitlab.mccinfo.net/cjcortney/recipe-app-styleguide",
+        frameworks: ["Angular", "Angular Material", "HTML", "CSS"]
+    },
 
-    {title: "Movie Database Site",
-    description: "project description",
-    url: "https://mcc-movies.herokuapp.com/",
-    imgUrl: "https://firebasestorage.googleapis.com/v0/b/jordan-clark-personal-site.appspot.com/o/images%2Fmovies.PNG?alt=media&token=1ea3fe28-62d7-4623-8599-2598cacbd766",
-    repoUrl: "https://gitlab.mccinfo.net/cjcortney/movies-project"      
-},
+    {
+        title: "MOVIE DATABASE",
+        description: "Spring Boot application that showcases movies, actors, credits.",
+        url: "https://mcc-movies.herokuapp.com/",
+        imgUrl: "https://firebasestorage.googleapis.com/v0/b/jordan-clark-personal-site.appspot.com/o/images%2Fmovies.PNG?alt=media&token=1ea3fe28-62d7-4623-8599-2598cacbd766",
+        repoUrl: "https://gitlab.mccinfo.net/cjcortney/movies-project",
+        frameworks: ["Java", "Spring Boot", "Bootstrap", "Thymeleaf"]
+    },
 
-    {title: "ReciMe",
-    description: "project description",
-    url: "https://dev.recipe.mcc-code-school.com/",
-    imgUrl: "https://firebasestorage.googleapis.com/v0/b/jordan-clark-personal-site.appspot.com/o/images%2Frecime.PNG?alt=media&token=afe272ca-a769-4467-b414-54eccb51d03b",
-    repoUrl: "https://gitlab.mccinfo.net/code-school/students/recipe-app"    
-},
+    {
+        title: "RECIME RECIPE SHARING",
+        description: "Final project for MCC Code School.  The app is designed to cut down on the fluff for users looking for recipes. Users are able to sort and filter recipes to find exactly what they are looking for, as well as create their own recipes and save to the database.",
+        url: "https://dev.recipe.mcc-code-school.com/",
+        imgUrl: "https://firebasestorage.googleapis.com/v0/b/jordan-clark-personal-site.appspot.com/o/images%2Frecime.PNG?alt=media&token=afe272ca-a769-4467-b414-54eccb51d03b",
+        repoUrl: "https://gitlab.mccinfo.net/code-school/students/recipe-app",
+        frameworks: ["Angular", "Angular Material", "HTML", "CSS"]
+    },
+    {
+        title: "PERSONAL SITE",
+        description: "Site dedicated to showcasing the work and experience of Jordan Clark (me!).",
+        url: "https://jordan-clark-personal-site.web.app/",
+        imgUrl: "https://firebasestorage.googleapis.com/v0/b/jordan-clark-personal-site.appspot.com/o/images%2Fpersonalsite.PNG?alt=media&token=36fa030c-41aa-445a-ab60-3a7ac2fa95fd",
+        repoUrl: "https://github.com/jaclark756/jordan-clark/tree/main",
+        frameworks: ["Firebase", "Angular", "Angular Material", "HTML", "CSS"]
+    },
 ]
 
