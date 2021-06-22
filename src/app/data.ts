6a489be0af9db7559a4eb19d9b55fc4a0f0e7f04
jaclark7 <jaclark7@mccneb.edu>
2021-06-22 14:05:04 -0500

    fixed portfolio site repo link

--- src/app/data.ts
+++ src/app/data.ts
@@ -48,7 +48,7 @@ export const PROJECTS = [
         description: "Site dedicated to showcasing the work and experience of Jordan Clark (me!).",
         url: "https://jordan-clark-personal-site.web.app/",
         imgUrl: "https://firebasestorage.googleapis.com/v0/b/jordan-clark-personal-site.appspot.com/o/images%2Fpersonalsite.PNG?alt=media&token=36fa030c-41aa-445a-ab60-3a7ac2fa95fd",
-        repoUrl: "https://github.com/jaclark756/jordan-clark/tree/main",
+        repoUrl: "https://github.com/jaclark756/jordan-clark",
         frameworks: ["Firebase", "Angular", "Angular Material", "HTML", "CSS"]
     },
 ]
